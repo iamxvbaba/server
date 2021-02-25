4bf6ee7e67fccc9f8ef7c8b155bdc9ea4f10d4ce
xuhui <944808666@qq.com>
2021-02-25 23:33:53 +0800

    fd

--- upgrader/dup_fd_windows.go
+++
@@ -1,7 +0,0 @@
-package upgrader
-
-import "errors"
-
-func dupFd(fd uintptr, name fileName) (*file, error) {
-	return nil, errors.New("upgrader: duplicating file descriptors is not supported on this platform")
-}
